c596dbea5342697dbab12e8d643c181a5b9be842
QuocDL <quoclcph18659@gmail.com>
2023-11-15 05:57:39 +0700

    change lap

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,28 +1,28 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 import { product } from '../type/type'
 import axios from 'axios'
 import ProductContent from './components/products'
 
 
-const HomePage = () =>{
+const HomePage = () => {
     const [products, setProducts] = useState<product[]>([])
-    const fetchProduct = async()=>{
-       try {
-            const {data} = await axios.get('https://fakestoreapi.com/products')
+    const fetchProduct = async () => {
+        try {
+            const { data } = await axios.get('https://fakestoreapi.com/products')
             setProducts(data)
-       } catch (error) {
+        } catch (error) {
             console.log('Loi');
-       }
+        }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         fetchProduct()
-    },[])
+    }, [])
     console.log('result', products);
-    return(
-         <div className='mt-[30px] text-center'>
+    return (
+        <div className='mt-[30px] text-center'>
             <div className='flex justify-center'>
-                <img className='rounded-[10px]' src="https://img.timviecthietke.com/2021/06/kich-thuoc-banner-website-1.png" alt="" />
+                <img className='rounded-[10px] px-[2%]' src="https://img.timviecthietke.com/2021/06/kich-thuoc-banner-website-1.png" alt="" />
             </div>
             <div className=''>
                 <ul>
@@ -32,11 +32,11 @@ const HomePage = () =>{
                     <li><a href=""></a></li>
                 </ul>
             </div>
-                <div className=" mt-[30px] flex gap-[2%] gap-y-7 w-[100%] flex-wrap justify-center items-center mb-[50px]">
-                    {products.map((product, index) => (
-                            <ProductContent key={index} product={product} />
-                    ))}
-                </div>
+            <div className=" mt-[30px] flex gap-[2%] gap-y-7 w-[100%] flex-wrap justify-center items-center mb-[50px]">
+                {products.map((product, index) => (
+                    <ProductContent key={index} product={product} />
+                ))}
+            </div>
         </div>
     )
 }
